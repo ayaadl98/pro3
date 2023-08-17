378a9852d8b550fee9c3a26c87263caafc8b417a
aya al-adl <ayaaladl98@gmail.com>
2023-08-18 00:10:41 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+
+//  document.getElementById("aya").onclick = (function button (){
+//     alert("watch out")
+ 
+// })
+
+// function test (){
+//     alert('fhhfjfjfgfgjhf');
+// }
+x.S
+
+   
